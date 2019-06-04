bbd961351ab6b9ee056aeaac08335aeb7467f9c0
Sebastian Kisiel <UnbreakableBonee>
2019-06-04 19:00:26 +0200

    wish and category class

---
+++ app/src/main/java/com/example/wishapp/AddCategoryActivity.kt
@@ -0,0 +1,23 @@
+package com.example.wishapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.support.v7.app.AppCompatActivity
+import android.widget.EditText
+
+class AddCategoryActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+
+    fun addCategory(view: View) {
+        val nameEditText = findViewById<EditText>(R.id.editText4)
+        val name = nameEditText.text.toString()
+        val descriptionEditText = findViewById<EditText>(R.id.editText5)
+        val description = descriptionEditText.text.toString()
+        // add category to db
+    }
+}
---
+++ app/src/main/java/com/example/wishapp/Category.kt
@@ -0,0 +1,11 @@
+package com.example.wishapp
+
+class Category{
+    var name: String = ""
+    var description: String = ""
+
+    constructor(_name: String, _description: String) {
+        name = _name;
+        description = _description;
+    }
+}
