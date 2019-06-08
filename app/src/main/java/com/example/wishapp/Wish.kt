7f23e32bf055decc1630ce1141a15cefc9250208
Bartosz Stocki <bartek_stocki@wp.pl>
2019-06-08 05:15:05 +0200

    added saveWish method and ListView with wishList

--- app/src/main/java/com/example/wishapp/Wish.kt
+++ app/src/main/java/com/example/wishapp/Wish.kt
@@ -1,15 +1,8 @@
 package com.example.wishapp
 
-class Wish {
-    var name: String = ""
-    var description: String = ""
-    var category: Category = Category("", "")
-    var imagePath: String = ""
-
-    constructor(_name: String, _description: String, _category: Category, _imagePath: String) {
-        name = _name
-        description = _description
-        category = _category
-        imagePath = _imagePath
-    }
-}
+class Wish (
+//    var id: Int,
+    var name: String,
+    var description: String,
+//    var category: Category = Category("", "")
+    var imagePath: String?)
