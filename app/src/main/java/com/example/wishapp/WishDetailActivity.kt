d89286d4c03ddd8509634603b18a222885de73b0
Bartosz Stocki <bartek_stocki@wp.pl>
2019-06-09 08:29:58 +0200

    Added edit wish in dialog window.

--- app/src/main/java/com/example/wishapp/WishDetailActivity.kt
+++ app/src/main/java/com/example/wishapp/WishDetailActivity.kt
@@ -1,11 +1,14 @@
 package com.example.wishapp
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_wish_detail.*
 
@@ -19,70 +22,99 @@ class WishDetailActivity : AppCompatActivity() {
         val databaseHandler: DatabaseHandler = DatabaseHandler(this)
         val wish = databaseHandler.getWishList().get(wishId)
 
-        val wish_name_display = findViewById<EditText>(R.id.wish_name_display).apply {
-            setText(wish.name)
+        val wish_name_display = findViewById<TextView>(R.id.wish_name_display).apply {
+            text = wish.name
         }
 
-        val wish_desc_display = findViewById<EditText>(R.id.wish_desc_display).apply {
-            setText(wish.description)
+        val wish_desc_display = findViewById<TextView>(R.id.wish_desc_display).apply {
+            text = wish.description
         }
 
         val wishDetailImage = findViewById<ImageView>(R.id.wish_detail_image).apply {
             setImageURI(Uri.parse(Uri.decode(wish.imagePath)))
         }
 
-        button_find.setOnClickListener{
+        button_find.setOnClickListener {
 
         }
 
-        button_save_edited.setOnClickListener{
-            saveWish()
+        button_save_edited.setOnClickListener {
+            editWish(wish)
         }
 
-        button_delete_wish.setOnClickListener{
+        button_delete_wish.setOnClickListener {
             deleteWish(wish)
         }
+    }
 
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+    }
 
-
+    fun dispatchSaveEditedWishIntent() {
 
     }
 
-    fun dispatchSaveEditedWishIntent(){
+    fun dispatchDeleteWishIntent() {
 
     }
 
-    fun dispatchDeleteWishIntent(){
-
-    }
-
-    fun searchImage(imgPath : String) {
-        var base_url : String  = "https://www.google.com/searchbyimage?site=search&sa=X&image_url="
-        var google_shop : String = "https://www.google.com/search?q={}&source=lnms&tbm=shop&"
+    fun searchImage(imgPath: String) {
+        var base_url: String = "https://www.google.com/searchbyimage?site=search&sa=X&image_url="
+        var google_shop: String = "https://www.google.com/search?q={}&source=lnms&tbm=shop&"
             .format("slowo + klucz")
         val uri = Uri.parse(base_url + imgPath)
-        var intent : Intent = Intent(Intent.ACTION_VIEW, uri)
+        var intent: Intent = Intent(Intent.ACTION_VIEW, uri)
         startActivity(intent)
     }
 
-    fun saveWish(){
-        val message = Toast.makeText(applicationContext, "Changes saved", Toast.LENGTH_LONG)
-        message.show()
-
-        // zapisuje edytowane zmainy, zostaje w tym samym widoku
-    }
-
     fun deleteWish(wish: Wish) {
-        val databaseHandler: DatabaseHandler= DatabaseHandler(this)
+        val databaseHandler: DatabaseHandler = DatabaseHandler(this)
 
         val status = databaseHandler.deleteWish(wish)
-        if(status > -1){
-                Toast.makeText(applicationContext,"Wish deleted.",Toast.LENGTH_LONG).show()
+        if (status > -1) {
+            Toast.makeText(applicationContext, "Wish deleted.", Toast.LENGTH_LONG).show()
             finish()
-        }else{
-            Toast.makeText(applicationContext,"Something went wrong.",Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(applicationContext, "Something went wrong.", Toast.LENGTH_LONG).show()
         }
-
     }
 
+    fun editWish(wish: Wish) {
+        val dialogBuilder = AlertDialog.Builder(this)
+        val inflater = this.layoutInflater
+        val dialogView = inflater.inflate(R.layout.update_dialog, null)
+        dialogBuilder.setView(dialogView)
+
+        val wishName = dialogView.findViewById(R.id.updateName) as EditText
+        wishName.setText(wish.name)
+        val wishDescription = dialogView.findViewById(R.id.updateDescription) as EditText
+        wishDescription.setText(wish.description)
+
+        dialogBuilder.setTitle("Wish update")
+        dialogBuilder.setMessage("Enter data below")
+        dialogBuilder.setPositiveButton("Update", DialogInterface.OnClickListener { _, _ ->
+
+            val updateName = wishName.text.toString()
+            val updateDescription = wishDescription.text.toString()
+            val databaseHandler: DatabaseHandler = DatabaseHandler(this)
+            if (updateName.trim() != "" && updateDescription.trim() != "") {
+                val updatedWish = wish
+                updatedWish.name = updateName
+                updatedWish.description = updateDescription
+                val status = databaseHandler.updateWish(updatedWish)
+                if (status > -1) {
+                    Toast.makeText(applicationContext, "Wish updated", Toast.LENGTH_LONG).show()
+                }
+            } else {
+                Toast.makeText(applicationContext, "Name or description cannot be blank", Toast.LENGTH_LONG).show()
+            }
+            finish()
+            startActivity(getIntent())
+        })
+        dialogBuilder.setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, which ->
+        })
+        val b = dialogBuilder.create()
+        b.show()
+    }
 }
